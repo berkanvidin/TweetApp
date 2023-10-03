b3dcc0e0cab29c1a3e899659e673dd467f447bab
Berkan Vidin <mehmetberkanvdn@outlook.com>
2023-10-03 13:56:45 +0300

    project added

---
+++ tweetapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+app_name = 'tweetapp'
+urlpatterns = [
+    path('',views.listtweet,name='listtweet'),
+    path('addtweet/',views.addtweet,name='addtweet'),
+    path('addtweetbyform',views.addTweetByForm,name="addtweetbyform"),
+    path('addtweetbymodelform',views.addtweetbymodelform,name="addtweetbymodelform"),
+    path('signup',views.SignUpView.as_view(),name='signup'),
+    path('deletetweet/<int:id>',views.deletetweet,name='deletetweet')
+]
